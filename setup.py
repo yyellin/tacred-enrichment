74663db8d9b42f4544a948785df9fa7b728834fb
jyellin <yyellin@gmail.com>
2020-01-03 18:37:26 +0200

    removed script folder - unnecessary

--- setup.py
+++ setup.py
@@ -11,18 +11,5 @@ setup(
     description='Various utilities for processing and analyzing relation extraction related data',
     install_requires=[
         'stanfordnlp', 'nltk', 'pandas', 'networkx', 'spacy', 'tupa', 'ucca', 'requests', 'stanfordcorenlp', 'ijson'
-    ],
-    scripts=[
-        'bin/tac_to_csv',
-        'bin/parse_ud',
-        'bin/parse_ucca',
-        'bin/parse_ucca2',
-        'bin/append_ner',
-        'bin/append_pss',
-        'bin/extract_relations_ud',
-        'bin/extract_relations_ucca',
-        'bin/extract_relations_ud_plus_ucca',
-        'bin/filter_relations',
-
     ]
 )
