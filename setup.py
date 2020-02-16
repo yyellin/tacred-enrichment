110a74bf57a57727a53493ac7505d5440fd39e38
yyellin <yyellin@gmail.com>
2020-02-16 13:49:25 +0200

    add missing dependencies (and up the version to 0.0.6)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='path_to_re',
-    version='0.0.5',
+    version='0.0.6',
     packages=['path_to_re', 'path_to_re.internal'],
     url='https://github.com/yyellin/relation_extraction_utils',
     license='GPLv3',
@@ -10,6 +10,6 @@ setup(
     author_email='yyellin@gmail.com',
     description='Various utilities for processing and analyzing relation extraction related data',
     install_requires=[
-        'stanfordnlp', 'nltk', 'pandas', 'networkx', 'spacy', 'tupa', 'ucca', 'requests', 'stanfordcorenlp', 'ijson'
+        'pytorch_pretrained_bert', 'more_itertools', 'stanfordnlp', 'nltk', 'pandas', 'networkx', 'spacy', 'tupa', 'ucca', 'requests', 'stanfordcorenlp', 'ijson'
     ]
 )
