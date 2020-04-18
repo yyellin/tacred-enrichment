d6f6f829aef7836c3c67f637f25efef7625889c8
yyellin <yyellin@gmail.com>
2020-04-18 13:24:20 +0300

    support get_ucca_encodings_on_paths option

--- path_to_re/gcn/enhance_tac.py
+++ path_to_re/gcn/enhance_tac.py
@@ -2,6 +2,7 @@
 
 Usage:
   enhance_tac.py get_ucca_words_on_paths <tupa_module_path>  [--input=<input-file>] [--output=<output-file>]
+  enhance_tac.py get_ucca_encodings_on_paths <tupa_module_path>  [--input=<input-file>] [--output=<output-file>]
   enhance_tac.py get_ucca_paths <tupa_module_path>  [--input=<input-file>] [--output=<output-file>]
   enhance_tac.py get_ud_paths <corenlp_server> <corenlp_port> [--input=<input-file>] [--output=<output-file>]
   enhance_tac.py get_ucca_paths <tupa_module_path> get_ud_paths <corenlp_server> <corenlp_port> [--input=<input-file>] [--output=<output-file>]
@@ -113,7 +114,9 @@ def get_ucca_words_on_path(sentence, tac , parser):
     def compare_by(terminal_list, one, another):
         return len(terminal_list)
 
-    terminals_of_path = graph.get_terminals_for_subgraph(ent1_parent_node_id, ent2_parent_node_id, compare_by)
+    subtree = graph.get_minimal_subgraph(ent1_parent_node_id, ent2_parent_node_id, compare_by)
+    terminals_of_path = subtree.get_terminals()
+
     terminals_in_path = sorted([int(i.split('.')[1]) - 1 for i in terminals_of_path])
 
     reverse_token_map = MapTokenization.map_a_to_b(ucca_tokens, tac['token'])
@@ -123,6 +126,70 @@ def get_ucca_words_on_path(sentence, tac , parser):
             tac_terminals_in_path.append(tac_index)
 
     return tac_terminals_in_path;
+
+def get_ucca_encodings_on_path(sentence, tac , parser):
+
+    parsed_sentence = parser.parse_sentence(sentence)
+    if parsed_sentence is None:
+        print('failed to perform UCCA parse')
+        return None
+
+    ucca_tokens = [ucca_terminal.text for ucca_terminal in parsed_sentence.terminals]
+    token_map = MapTokenization.map_a_to_b(tac['token'], ucca_tokens)
+
+    if tac['subj_start'] not in token_map \
+            or tac['subj_end'] not in token_map \
+            or tac['obj_start'] not in token_map \
+            or tac['obj_end'] not in token_map:
+        print('failed to map "tac tokenization" to "ucca tokenization"')
+        return None
+
+    ent1_start = token_map[tac['subj_start']][0] + 1
+    ent2_start = token_map[tac['obj_start']][0] + 1
+
+
+    links = parsed_sentence.get_links()
+
+    ent1_start_node_id = parsed_sentence.get_node_id_by_token_id(ent1_start)
+    ent1_parent_node_ids = Link.get_parents(links, ent1_start_node_id)
+    if len(ent1_parent_node_ids) == 0:
+        print('trouble identifying start node for ent1')
+        return None
+    ent1_parent_node_id = ent1_parent_node_ids[0]
+
+    ent2_start_node_id = parsed_sentence.get_node_id_by_token_id(ent2_start)
+    ent2_parent_node_ids = Link.get_parents(links, ent2_start_node_id)
+    if len(ent2_parent_node_ids) == 0:
+        print('trouble identifying start node for ent2')
+        return None
+    ent2_parent_node_id = ent2_parent_node_ids[0]
+
+    graph = DepGraph(links, is_terminal)
+
+    def compare_by(terminal_list, one, another):
+        return len(terminal_list)
+
+    subtree = graph.get_minimal_subgraph(ent1_parent_node_id, ent2_parent_node_id, compare_by)
+    terminals_on_path = subtree.get_terminals()
+
+    terminals_on_path = sorted(terminals_on_path, key = lambda t : int(t.split('.')[1]))
+
+    terminals_of_path_to_ucca_encoding = {}
+    for terminal_on_path in terminals_on_path:
+        token_index = int(terminal_on_path.split('.')[1]) - 1
+        steps = subtree.get_undirected_steps(terminal_on_path, subtree.root())
+        terminals_of_path_to_ucca_encoding[token_index] = Step.get_dependency_representation(steps[1:])
+
+    reverse_token_map = MapTokenization.map_a_to_b(ucca_tokens, tac['token'])
+
+    tac_terminals_of_path_to_ucca_encoding = {}
+    for terminal_in_path, encoding in terminals_of_path_to_ucca_encoding.items():
+        for tac_index in reverse_token_map[terminal_in_path]:
+            tac_terminals_of_path_to_ucca_encoding[tac_index] = encoding
+
+
+
+    return tac_terminals_of_path_to_ucca_encoding;
 
 
 
@@ -167,14 +234,14 @@ def get_ud_path(sentence, tac, core_nlp):
     return steps_representation
 
 
-def enhance_tag(input_stream, output_stream, get_ud_paths, corenlp_server, corenlp_port, get_ucca_paths, get_ucca_words_on_paths, model_prefix):
+def enhance_tag(input_stream, output_stream, get_ud_paths, corenlp_server, corenlp_port, get_ucca_paths, get_ucca_words_on_paths, get_ucca_encodings_on_paths, model_prefix):
 
     json_read = ijson.items(input_stream, 'item')
     detokenizer = Detokenizer()
 
     with jsonlines.Writer(output_stream) as json_write:
 
-        if get_ucca_paths or get_ucca_words_on_paths:
+        if get_ucca_paths or get_ucca_words_on_paths or get_ucca_encodings_on_paths:
             parser = TupaParser(model_prefix)
 
         if get_ud_paths:
@@ -195,6 +262,10 @@ def enhance_tag(input_stream, output_stream, get_ud_paths, corenlp_server, coren
             if get_ucca_words_on_paths:
                 ucca_words_on_path = get_ucca_words_on_path(sentence, item, parser)
                 item['ucca_words_on_path'] = ucca_words_on_path
+
+            if get_ucca_encodings_on_paths:
+                ucca_encodings_on_path = get_ucca_encodings_on_path(sentence, item, parser)
+                item['ucca_encodings_on_path'] = ucca_encodings_on_path
 
 
             if get_ud_paths:
@@ -226,13 +297,14 @@ if __name__ == "__main__":
 
     get_ucca_paths = args.get('get_ucca_paths', False)
     get_ucca_words_on_paths = args.get('get_ucca_words_on_paths', False)
+    get_ucca_encodings_on_paths = args.get('get_ucca_encodings_on_paths', False)
     tupa_module_path = args.get('<tupa_module_path>', None)
 
 
     # https://stackoverflow.com/questions/14207708/ioerror-errno-32-broken-pipe-python
     revert_to_default_behaviour_on_sigpipe()
 
-    enhance_tag(input_stream, output_stream, get_ud_paths, corenlp_server, corelnlp_port, get_ucca_paths, get_ucca_words_on_paths, tupa_module_path)
+    enhance_tag(input_stream, output_stream, get_ud_paths, corenlp_server, corelnlp_port, get_ucca_paths, get_ucca_words_on_paths, get_ucca_encodings_on_paths, tupa_module_path)
 
 
     #assign_ucca_tree(input, output)
