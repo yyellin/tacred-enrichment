ef005ae05b4bbd7aa0bb5bd6e28e72effd35f683
yyellin <yyellin@gmail.com>
2020-04-29 12:35:23 +0300

    version 0.0.10 :)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='path_to_re',
-    version='0.0.9',
+    version='0.0.10',
     packages=['path_to_re', 'path_to_re.extra', 'path_to_re.internal', 'path_to_re.gcn'],
     url='https://github.com/yyellin/relation_extraction_utils',
     license='GPLv3',
