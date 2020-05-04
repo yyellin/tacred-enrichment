ce3dd43f4f59a65b8db8852d0300692385292943
yyellin <yyellin@gmail.com>
2020-05-04 09:01:37 +0300

    UCCA head ids also needed to be mapped to tacred ids - fixing this

--- path_to_re/gcn/enhance_tac.py
+++ path_to_re/gcn/enhance_tac.py
@@ -192,7 +192,7 @@ def get_ucca_heads(parsed_sentence, tac):
     reverse_token_map = MapTokenization.map_a_to_b(ucca_tokens, sanitized_tac_tokens)
 
     #ensure we have coverage over all of tack['token']
-    tac_coverage = {tac_token:True for tac_token in [tac_tokens for tac_tokens in reverse_token_map.values()]}
+    tac_coverage = {tac_token:True for tac_tokens in reverse_token_map.values() for tac_token in tac_tokens}
     if (len(tac_coverage) != len(tac['token'])) or (0 not in tac_coverage) or ((len(tac_coverage)-1) not in tac_coverage):
         print('failed to allign all UCCA and TACRED tokens for UCCA head/dep extraction')
         return (None, None)
