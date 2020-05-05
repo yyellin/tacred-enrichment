c51f94f0682d70f873970129b5f265a94620a6ff
yyellin <yyellin@gmail.com>
2020-05-05 08:32:53 +0300

    support skip flag

--- path_to_re/extra/lines_of_json_to_json.py
+++ path_to_re/extra/lines_of_json_to_json.py
@@ -1,7 +1,7 @@
 """Lines of json to json
 
 Usage:
-  lines_of_json_to_json.py [--input=<input-file>] [--output=<output-file>]
+  lines_of_json_to_json.py [--skip=<fields-to-skip>] [--input=<input-file>] [--output=<output-file>]
 
 Options:
   -h --help     Show this screen.
@@ -18,10 +18,14 @@ if __name__ == "__main__":
 
     input_stream = open(args['--input'], encoding='utf-8') if args['--input'] is not None else sys.stdin
     output_stream = open(args['--output'], 'w', encoding='utf-8', newline='', buffering=1) if args['--output'] is not None else sys.stdout
+    fields_to_skip = [field for field in args['--skip'].split(',')] if args['--skip'] is not None else []
 
     entries = []
     with jsonlines.Reader(input_stream) as json_reader:
         for entry in json_reader:
+            for field_to_skip in fields_to_skip:
+                del entry[field_to_skip]
+
             entries.append(entry)
 
     with output_stream:
