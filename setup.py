8ecf417a932163c7f8ad04ad9fb9f0b5c2427e15
yyellin <yyellin@gmail.com>
2021-06-25 13:44:24 +0300

    fixed setup.py metadata

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='tacred_enrichment',
     version='1.0.0',
     packages=['tacred_enrichment', 'tacred_enrichment.extra', 'tacred_enrichment.internal'],
-    url='https://github.com/yyellin/relation_extraction_utils',
-    license='GPLv3',
-    author='yyellin',
-    author_email='yyellin@gmail.com',
-    description='Various utilities for processing and analyzing relation extraction related data',
+    url='https://github.com/yyellin/tacred-enrichment',
+    license='Apache 2.0',
+    author='Jonathan Yellin',
+    author_email='jonathan.yellin@mail.huji.ac.il',
+    description='A set of modules for supplementing the TACRED dataset with additional attributes, to be used by downstream RE neural networks',
     install_requires=[
         'conllu', 'pytorch_pretrained_bert', 'jsonlines', 'docopt', 'more_itertools', 'stanfordnlp', 'nltk', 'pandas', 'networkx', 'spacy', 'tupa==1.4.2', 'ucca==1.2.3', 'requests', 'stanfordcorenlp', 'ijson'
     ]
